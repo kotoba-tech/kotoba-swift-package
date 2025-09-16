a47735cd1691e81e88d9a9db890601636f3fae19
Justin Garcia <justin.garcia@gmail.com>
2025-09-16 18:11:41 -0400

    1.0.13

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "kotoba_tts",
-            url: "https://github.com/kotoba-tech/kotoba-swift-package/releases/download/1.0.12/kotoba_tts.xcframework.zip",
-            checksum: "d888dc71845ef16522d14cc0e6e52e4fcee5872fe5e77fb5cdce82fd6e392a34"
+            url: "https://github.com/kotoba-tech/kotoba-swift-package/releases/download/1.0.13/kotoba_tts.xcframework.zip",
+            checksum: "10097acdc314577d30531ed9c09846395c754b51a9da9785dfd70d4391b889ff"
         )
     ]
 )
