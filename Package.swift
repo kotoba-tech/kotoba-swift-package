cade6297547015349a26989b1366bd685fca26d5
Justin Garcia <justin.garcia@gmail.com>
2025-09-16 23:28:36 -0400

    1.0.16: fix: the previous release’s source didn’t have the latest changes

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "kotoba_tts",
-            url: "https://github.com/kotoba-tech/kotoba-swift-package/releases/download/1.0.15/kotoba_tts.xcframework.zip",
-            checksum: "74b2b9e37f92f975627655e85ea18c6ac72a2939e08b3610f4dd3d3d5d61bc60"
+            url: "https://github.com/kotoba-tech/kotoba-swift-package/releases/download/1.0.16/kotoba_tts.xcframework.zip",
+            checksum: "36402aa120dd427371ee68dfeff7e0271be7806c67c56949bf944bc98000103a"
         )
     ]
 )
