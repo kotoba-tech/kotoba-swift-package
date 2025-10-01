8b3a8fe6263d3502ce8ccb1b67b26f0ac51457a3
Justin Garcia <justin.garcia@gmail.com>
2025-10-01 10:45:53 -0400

    try to correct 1.0.21

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "kotoba_tts",
             url: "https://github.com/kotoba-tech/kotoba-swift-package/releases/download/1.0.21/kotoba_tts.xcframework.zip",
-            checksum: "145728dfa464a3248a886194f7e16727b7bfb7d5c0a84ddd2a87541d66473fe8"
+            checksum: "4c9b1cd11ae08f2ff152c2f0e4746f59c1208818e4bfe312402b8a56182fe59f"
         )
     ]
 )
